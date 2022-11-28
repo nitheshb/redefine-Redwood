d5cf65a50aed5fa239d36099bdaca1617278455b
batapatn <nithesh.batapati@cdk.com>
2022-11-28 08:11:05 +0530

    added try catch

--- web/src/context/firebaseConfig.ts
+++ web/src/context/firebaseConfig.ts
@@ -10,33 +10,48 @@ import { getStorage } from 'firebase/storage'
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: 'AIzaSyDlr5xlq378B5vyetXvE_rK1r4EhPhFTt4',
+//   authDomain: 'redefine-erp.firebaseapp.com',
+//   projectId: 'redefine-erp',
+//   storageBucket: 'redefine-erp.appspot.com',
+//   messagingSenderId: '805525655781',
+//   appId: '1:805525655781:web:dfa846398899fe3ddc840c',
+//   measurementId: 'G-C2BJ3N02SQ',
+// }
+
 const firebaseConfig = {
-  apiKey: 'AIzaSyDlr5xlq378B5vyetXvE_rK1r4EhPhFTt4',
-  authDomain: 'redefine-erp.firebaseapp.com',
-  projectId: 'redefine-erp',
-  storageBucket: 'redefine-erp.appspot.com',
-  messagingSenderId: '805525655781',
-  appId: '1:805525655781:web:dfa846398899fe3ddc840c',
-  measurementId: 'G-C2BJ3N02SQ',
-}
+  apiKey: "AIzaSyDlr5xlq378B5vyetXvE_rK1r4EhPhFTt4",
+  authDomain: "redefine-erp.firebaseapp.com",
+  projectId: "redefine-erp",
+  storageBucket: "redefine-erp.appspot.com",
+  messagingSenderId: "805525655781",
+  appId: "1:805525655781:web:ea23286df1c6911ddc840c",
+  measurementId: "G-XPGE6HF5G4"
+};
 
 const settings = {
   experimentalForceLongPolling: true,
 }
+let db, storage, auth, analytics
 // Initialize Firebase
+try {
+  const app = initializeApp(firebaseConfig)
+  // initializeFirestore(app, settings)
 
-const app = initializeApp(firebaseConfig)
-// initializeFirestore(app, settings)
+  // firebase.firestore().settings({ experimentalForceLongPolling: true });
+  // const db = getFirestore(app)
+   db = initializeFirestore(app, {
+    experimentalForceLongPolling: true,
+  })
+   storage = getStorage()
 
-// firebase.firestore().settings({ experimentalForceLongPolling: true });
-// const db = getFirestore(app)
-const db = initializeFirestore(app, {
-  experimentalForceLongPolling: true,
-})
-const storage = getStorage()
+  // export const auth = app.auth()
+   auth = getAuth()
+   analytics = getAnalytics(app)
+} catch (error) {
+console.log('error at auth', error)
+}
 
-// export const auth = app.auth()
-const auth = getAuth()
-const analytics = getAnalytics(app)
 
 export { auth, db, storage }
